d1451543abed1d608ef7979b5dfa92495de046fc
jxz <jxz@duke.edu>
2022-04-07 01:57:39 -0400

    Accompanying WeatherData struct

---
+++ WeatherData.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherData.swift
+//  TrailStar
+//
+//  Created by William Convertino on 3/27/22.
+//  Updated by Jeffrey Zhou on 4/1/22.
+
+struct WeatherData {
+    var city: String
+    var state: String
+    var country: String
+    
+    var date: String
+    
+    var temperature: Double
+    var conditions: String
+    var rainChance: Int
+    
+}
