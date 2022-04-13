afb5c65991e47d7740144452797a2d60362d15b0
jxz <jxz@duke.edu>
2022-04-13 15:30:15 -0400

    added comment to TrailSearchModule

--- TrailStar/TrailStar/BackEnd/SearchModules/TrailSearchModule.swift
+++ TrailStar/TrailStar/BackEnd/SearchModules/TrailSearchModule.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 class TrailSearchModule {
-    
+    //date must be in form: yyyy-mm-dd. For example, April 5th 2022 is: 2022-04-05
+    //days = number of days in advance that we are predicting. Can go from 0 to 2.
+    //0 means just today, 1 means tomorrow, and 2 means two days from now.
     static func getTrailResults(city: String, state: String, country: String, limit: Int=30, date: String, days: Int) throws -> [TrailSearchResult] {
         
         let trailList: [TrailData] = try TrailAPIModule.generateTrailList(city: city, state: state, country: country, limit: limit)
