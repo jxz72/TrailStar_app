ddcfac6b1cc9ca32200454b5dc887a30218f55b2
William Convertino <wac20@duke.edu>
2022-04-14 02:23:18 -0400

    Began integration of Trail API search with UI components

--- TrailStar/TrailStar/FrontEnd/HomepageViewController.swift
+++ TrailStar/TrailStar/FrontEnd/HomepageViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import CoreLocation
 
-class HomepageViewController: UIViewController {
+class HomepageViewController: UIViewController, CLLocationManagerDelegate {
 
     
     @IBOutlet weak var TrailImage1: UIImageView!
     @IBOutlet weak var TrailImage2: UIImageView!
     
+    @IBOutlet weak var Description1: UILabel!
+    
+    @IBOutlet weak var Description2: UILabel!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +28,45 @@ class HomepageViewController: UIViewController {
         TrailImage1.image = trailImages.randomElement()!;
         TrailImage2.image = trailImages.randomElement()!;
         
+        
+        let group = DispatchGroup()
+        group.enter()
+        
+        initializeLocationManagement();
+        group.leave()
+   
+        do {
+            
+            if (locationManager.location == nil){
+                throw APIError.connectionFailed
+            }
+            
+            var latitude: Float = Float(locationManager.location!.coordinate.latitude)
+            
+            var longitude: Float = Float(locationManager.location!.coordinate.longitude)
+            
+            let nearbyTrails: [TrailSearchResult] = try TrailSearchModule.getTrailResults(latitude: latitude, longitude: longitude, date: "2022-14-05", days: 0)
+        } catch {
+            print("Error: Couldn't find location");
+        }
+        
+        
+        print("Application Started");
         // Do any additional setup after loading the view.
+    }
+    
+    func initializeLocationManagement() {
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
     }
     
 
