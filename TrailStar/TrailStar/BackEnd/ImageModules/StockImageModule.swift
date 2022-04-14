52499ae233b82d1dc0968b6343c0a54c703d23d2
William Convertino <wac20@duke.edu>
2022-04-14 01:14:19 -0400

    Added ImageModules class to allow for image generation and added some stock images

--- TrailStar/TrailStar/BackEnd/ImageModules/StockImageModule.swift
+++ TrailStar/TrailStar/BackEnd/ImageModules/StockImageModule.swift
@@ -2,7 +2,56 @@
 //  StockImageModule.swift
 //  TrailStar
 //
-//  Created by student on 4/14/22.
+//  Created by William Convertino on 4/14/22.
 //
 
 import Foundation
+import UIKit
+
+class StockImageModule {
+    
+    static func getWeatherImages(weatherType: String) -> [UIImage] {
+        switch (weatherType) {
+            case "clear":
+            return clearImages;
+            
+            case "rain":
+            return rainImages;
+            
+            case "snow":
+            return snowImages;
+            
+            default:
+            return clearImages;
+        }
+    }
+    
+}
+
+var clearImages: [UIImage] = [
+    UIImage(named:"Clear1")!,
+    UIImage(named:"Clear2")!,
+    UIImage(named:"Clear3")!,
+    UIImage(named:"Clear4")!,
+    UIImage(named:"Clear5")!,
+    UIImage(named:"Clear6")!,
+    UIImage(named:"Clear7")!,
+    UIImage(named:"Clear8")!,
+    UIImage(named:"Clear9")!,
+    UIImage(named:"Clear10")!,
+    UIImage(named:"Clear11")!
+    
+]
+    
+var rainImages: [UIImage] = [
+    UIImage(named:"Rain1")!,
+    UIImage(named:"Rain2")!,
+    UIImage(named:"Rain3")!,
+    UIImage(named:"Rain4")!,
+    UIImage(named:"Rain5")!,
+    UIImage(named:"Rain6")!,
+    
+]
+var snowImages: [UIImage] = [
+    UIImage(named:"Snow1")!
+]
