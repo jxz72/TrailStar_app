d406e714ee20db61446b244178a2053765943924
Jiayi Hao <jiayi.hao@duke.edu>
2022-04-14 19:54:52 -0400

    Implemented date picker in SearchBar and constrained to three days

--- TrailStar/TrailStar/FrontEnd/UserInputViewController.swift
+++ TrailStar/TrailStar/FrontEnd/UserInputViewController.swift
@@ -9,33 +9,32 @@ import UIKit
 
 class UserInputViewController: UIViewController {
     
-    @IBOutlet weak var dateText : UITextField!
-    
-    let datePicker = UIDatePicker()
-    
+    @IBOutlet weak var dateTF: UITextField!
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    func createDatePicker() {
-        let toolbar = UIToolbar()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let datePicker = UIDatePicker()
+            datePicker.datePickerMode = .date
+            datePicker.addTarget(self, action: #selector(dateChange(datePicker:)), for: UIControl.Event.valueChanged)
+            datePicker.frame.size = CGSize(width: 0, height: 300)
+            datePicker.preferredDatePickerStyle = .wheels
+            //datePicker.datePickerMode = .date
+            datePicker.minimumDate = .now
+            datePicker.maximumDate = .init(timeIntervalSinceNow: 172800)
+            
+            dateTF.inputView = datePicker
+            dateTF.text = formatDate(date: Date()) // today's date
+           
+        }
         
+        @objc func dateChange(datePicker: UIDatePicker){
+            dateTF.text = formatDate(date: datePicker.date)
+        }
         
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        @objc func formatDate(date: Date)->String{
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MMMM dd yyyy"
+            return formatter.string(from: date)
+        }
 }
