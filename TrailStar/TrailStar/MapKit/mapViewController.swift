ff98947157f4e19e009459e1c65890aeb4834880
jxz <jxz@duke.edu>
2022-04-14 03:47:17 -0400

    Initial MapKit commit

---
+++ TrailStar/TrailStar/MapKit/mapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  mapkit_backend.swift
+//  TrailStar
+//
+//  Created by Jeffrey Zhou on 4/14/22.
+//
+
+import Foundation
+import MapKit
+
+class mapViewController{
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+}
+
