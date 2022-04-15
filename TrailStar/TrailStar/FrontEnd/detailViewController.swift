94e96c887c385614572ab2d1bbcdc9cb65304298
Yile Hu <yh322@duke.edu>
2022-04-15 15:30:41 -0400

    update and upload the detail view controller

---
+++ TrailStar/TrailStar/FrontEnd/detailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  detailViewController.swift
+//  detailPage
+//
+//  Created by Yile Hu on 4/14/22.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet weak var trailLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var discriptionText: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
