9362b3e7bc564ddcdab879515c3b65afc1d5a5ae
jxz <jxz@duke.edu>
2022-04-17 02:16:01 -0400

    Got MapKit to work in DetailView

--- TrailStar/TrailStar/FrontEnd/DetailedTrailViewController.swift
+++ TrailStar/TrailStar/FrontEnd/DetailedTrailViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class DetailedTrailViewController: UIViewController {
     
     @IBOutlet weak var trailName: UILabel!
     @IBOutlet weak var trailLocation: UILabel!
     @IBOutlet weak var trailLength: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     
     var selectedRow:Int!
     
@@ -29,17 +34,22 @@ class DetailedTrailViewController: UIViewController {
         trailLocation.text = "\(resultTrailList[selectedRow!].city),  \(resultTrailList[selectedRow!].state), USA)"
         trailLength.text = "\(resultTrailList[selectedRow!].length) miles"
         
+        
+        //Populate the mapkit
+        populateMap()
+        
         //WeatherAPI call
         do {
 
             let weatherForTrail: WeatherData = try WeatherAPI.generateWeather(trailData: resultTrailList[selectedRow!], date: searchDate, days: searchDays)
-        
         }
         catch{
             print("error: \(error)")
         }
         
     }
+    
+     
     
 
     /*
@@ -51,5 +61,45 @@ class DetailedTrailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
 
+}
+
+
+extension DetailedTrailViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    //var locationManager = CLLocationManager()
+    
+    func populateMap() {
+        print("populate map entered")
+
+        // Do any additional setup after loading the view.
+        //locationManager.delegate = self
+        mapView.delegate = self
+        //locationManager.requestWhenInUseAuthorization()
+        
+        //let trailLat = 36.001678
+        //let trailLong = -78.939767
+        let trailLat = 34.9387279
+        let trailLong = -82.2270568
+        
+        
+        let trailCoords = CLLocationCoordinate2D(latitude: trailLat,
+            longitude: trailLong)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: trailCoords, span: span)
+        
+        mapView.setRegion(region, animated: true)
+       
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = trailCoords
+        annotation.title = resultTrailList[selectedRow!].name
+        //annotation.subtitle = "\(resultTrailList[selectedRow!].length) miles"
+        mapView.addAnnotation(annotation)
+
+    
+    }
+    
 }
