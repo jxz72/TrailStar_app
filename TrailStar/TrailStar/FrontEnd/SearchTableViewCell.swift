0983459d9b519667fde05845cd9ed70e0e402852
jxz <jxz@duke.edu>
2022-04-17 23:29:06 -0400

    Fixed display of Trails in search table cells

--- TrailStar/TrailStar/FrontEnd/SearchTableViewCell.swift
+++ TrailStar/TrailStar/FrontEnd/SearchTableViewCell.swift
@@ -6,6 +6,7 @@ class SearchTableViewCell: UITableViewCell {
 
     @IBOutlet weak var trailName: UILabel!
     @IBOutlet weak var trailLocation: UILabel!
+    @IBOutlet weak var trailLength: UILabel!
     
     @IBOutlet weak var trailImage: UIImageView!
     
